f8d29f08932384f1911ad94c87e6bc96acee4467
RafiiProject <92308498+RafiiProject@users.noreply.github.com>
2024-05-29 13:31:29 +0700

    tambah fitur login

---
+++ src/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "store-admin-7f062.firebaseapp.com",
+  projectId: "store-admin-7f062",
+  storageBucket: "store-admin-7f062.appspot.com",
+  messagingSenderId: "2264576106",
+  appId: "1:2264576106:web:21127cfe3758f472c11a13"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
